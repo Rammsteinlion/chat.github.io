a49046d4bbe74c7493cf26df334ac1d31d5ec7d3
Elkin Murillo <rastaw27@gmail.com>
2024-12-19 23:22:57 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const chatContainer = document.getElementById("chat");
+const messageInput = document.getElementById("inputMessage");
+const sendMessageButton = document.getElementById("sendMessage");
+
+let typingTimeout;
+
+sendMessageButton.addEventListener("click", () => sendMessage('send'));
+messageInput.addEventListener("keydown", (event) => {
+    if (event.key === 'Enter') sendMessage('send');
+});
+
+// Simulación de recepción de mensajes (puedes usar websockets o APIs)
+setInterval(() => {
+    simulateReceivedMessage("Hola, este es un mensaje recibido.");
+}, 5000);   
+
+// Función para enviar un mensaje
+function sendMessage(type) {
+    const message = messageInput.value.trim();
+    if (message === '') return;
+
+    addMessageToChat(message, type);
+    messageInput.value = ''; // Limpiar el campo de entrada
+    scrollToBottom(); // Desplazar hacia el final del chat
+}
+
+// Función para agregar un mensaje al chat
+function addMessageToChat(content, type) {
+    const messageElement = document.createElement("div");
+    messageElement.classList.add("msg_cotainer", ); // 'send' o 'rcvd'
+    messageElement.textContent = content;
+    chatContainer.appendChild(messageElement);
+}
+
+// Simular la recepción de mensajes
+function simulateReceivedMessage(content) {
+    addMessageToChat(content, 'rcvd');
+    scrollToBottom();
+}
+type
+// Función para desplazar el chat hacia el final
+function scrollToBottom() {
+    chatContainer.scrollTop = chatContainer.scrollHeight;
+}
